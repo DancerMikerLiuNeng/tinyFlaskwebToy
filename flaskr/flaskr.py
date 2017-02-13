1564f3a2c283cfbabd93266258d090f767184782
DancerMikerLiuNeng <359373468@qq.com>
2017-02-13 17:44:10 +0800

    git test

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -3,7 +3,7 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-
+''' git test'''
 app = Flask(__name__)
 app.config.from_object(__name__)
 
